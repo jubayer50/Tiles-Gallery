0133c66d2dd7e14b56a2480e4c8d7e3af116d1eb
jubayer50 <aujubayer1@gmail.com>
2026-05-02 13:24:54 +0600

    search functionality applied

---
+++ src/Components/AllTiles/Search.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Button } from "@heroui/react";
+import React, { useState } from "react";
+import TilesCard from "../Ui/TilesCard";
+
+const Search = ({ tiles }) => {
+  const [search, setSearch] = useState("");
+  const [filteredTiles, setFilteredTiles] = useState(tiles);
+
+  const handleSearchBtn = () => {
+    const searchTile = tiles.filter((tile) =>
+      tile.title.toLowerCase().includes(search.toLowerCase()),
+    );
+
+    setFilteredTiles(searchTile);
+  };
+
+  return (
+    <div>
+      <div className="bg-[#FF653F20] py-2 px-3 rounded-md max-w-200 mx-auto flex items-center justify-between gap-4 mt-5">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          className="py-1.5 px-4 flex-1 border border-[#FF653F] rounded-md"
+          type="text"
+          placeholder="Search tiles"
+        />
+        <Button
+          onClick={handleSearchBtn}
+          className={"bg-[#FF653F] py-2 rounded-md text-white"}
+        >
+          Search
+        </Button>
+      </div>
+
+      <div className="mt-5 md:mt-7 grid md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {filteredTiles.map((tile) => (
+          <TilesCard key={tile.id} tile={tile}></TilesCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -1,6 +1,6 @@
-import TilesCard from "@/Components/Ui/TilesCard";
+import Search from "@/Components/AllTiles/Search";
+
 import { tilesData } from "@/lib/fetchData";
-import { Button } from "@heroui/react";
 
 const AllTilesPage = async () => {
   const tiles = await tilesData();
@@ -15,22 +15,7 @@ const AllTilesPage = async () => {
         </p>
       </div>
 
-      <div className="bg-[#FF653F20] py-2 px-3 rounded-md max-w-200 mx-auto flex items-center justify-between gap-4 mt-5">
-        <input
-          className="py-1.5 px-4 flex-1 border border-[#FF653F] rounded-md"
-          type="text"
-          placeholder="Search tiles"
-        />
-        <Button className={"bg-[#FF653F] py-2 rounded-md text-white"}>
-          Search
-        </Button>
-      </div>
-
-      <div className="mt-5 md:mt-7 grid md:grid-cols-3 lg:grid-cols-4 gap-5">
-        {tiles.map((tile) => (
-          <TilesCard key={tile.id} tile={tile}></TilesCard>
-        ))}
-      </div>
+      <Search tiles={tiles}></Search>
     </div>
   );
 };
